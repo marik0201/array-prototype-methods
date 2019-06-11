90217ea61c9533f4b2554244d4e042e3b459048d
marik0201 <31615728+marik0201@users.noreply.github.com>
2019-06-11 10:52:14 +0300

    impove methods

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-Array.prototype.select = function(cb) {
-  let selected = [];
-
-  for (const key in this) {
-    cb(this[key], key, this) && selected.push(this[key]);
-  }
-
-  return selected;
-};
-
-Array.prototype.calc = function() {
-  let sum = 0;
-
-  for (const key of this) {
-    if (typeof key === "number" && !isNaN(key)) {
-      sum += key;
-    } else {
-      return null;
-    }
-  }
-
-  return sum;
-};
-
-Array.prototype.unpack = function() {
-  let unpacked = [];
+Array.prototype.select = function(callback) {
+    const selected = [];
   
-  for (const key of this) {
-    if (key instanceof Array) {
-      unpacked = unpacked.concat(key.unpack());
-    } else {
-      unpacked.push(key);
+    for (const item of this) {
+      if (callback(item)) {
+          selected.push(item)
+      }
     }
-  }
-
-  return unpacked;
-};
-
-
-
-// TEST
-console.log([1, 2, 3, 4, 5].select(x => x > 2)); // [3,4,5]
-
-console.log([1, 2, 3, 4, 5].calc()); //15
-console.log([1, 2, 3, "Oh hi", 5].calc()); //null
-
-console.log([[1, [2,3,[4,5]]],[6,7,[8,9],[10]]].unpack()); //  [1,2,3,4,5,6,7,8,9,10]
-
+  
+    return selected;
+  };
+  
+  Array.prototype.calc = function() {
+    let sum = 0;
+  
+    for (const item of this) {
+      if (typeof item === "number" && !isNaN(item)) {
+        sum += item;
+      } else {
+        return null;
+      }
+    }
+  
+    return sum;
+  };
+  
+  Array.prototype.unpack = function(depth=1) {
+    let unpacked = [];
+    
+    for (const item of this) {
+      if (Array.isArray(item) && depth > 0) {
+        unpacked = unpacked.concat(item.unpack(depth - 1));
+      } else {
+        unpacked.push(item);
+      }
+    }
+  
+    return unpacked;
+  };
+  
+  
+  
+  // TEST
+  console.log([1, 2, 3, 4, 5].select(x => x > 2)); // [3,4,5]
+  
+  console.log([1, 2, 3, 4, 5].calc()); //15
+  console.log([1, 2, 3, "Oh hi", 5].calc()); //null
+  
+  console.log([[1, [2,3,[4,5]]],[6,7,[8,9],[10]]].unpack()); // [ 1, [ 2, 3, [ 4, 5 ] ], 6, 7, [ 8, 9 ], [ 10 ] ]
+  console.log([[1, [2,3,[4,5]]],[6,7,[8,9],[10]]].unpack(3)); //  [1,2,3,4,5,6,7,8,9,10]
+  
+  
